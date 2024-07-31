326ddc6abe9c85d5cd888f49026c2a4698436b51
j0rbe3n <jero.guzman@gmail.com>
2024-07-31 15:26:36 -0700

    fix Eventos

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import User, Church, Role
+from .models import User, Church, Role, Event, Article, ArticleImage, EventImage, ArticleComment, EventComment, EventAttendee
 
 admin.site.register(User)
 admin.site.register(Church)
 admin.site.register(Role)
+admin.site.register(Event)
+admin.site.register(Article)
+admin.site.register(ArticleImage)
+admin.site.register(EventImage)
+admin.site.register(ArticleComment)
+admin.site.register(EventComment)
+admin.site.register(EventAttendee)
 
 # Register your models here.
